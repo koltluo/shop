488d367eed20e14deae2bb6d30f07bc103c3f244
koltluo <koltluo999@gmail.com>
2025-03-21 16:02:03 +0800

    fix esbuild

--- cloudflare-worker/build.js
+++ cloudflare-worker/build.js
@@ -6,5 +6,5 @@ esbuild.build({
   outfile: 'dist/index.js',     // 输出文件
   platform: 'neutral',          // Cloudflare Worker 平台
   format: 'esm',                // 使用 ES 模块格式
-  external: ['__STATIC_CONTENT_MANIFEST'], // 忽略 Cloudflare 特定的全局变量
+  external: ['alipay-sdk'],     // 将 alipay-sdk 标记为外部依赖
 }).catch(() => process.exit(1));
