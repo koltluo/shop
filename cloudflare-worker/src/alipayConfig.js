7362352b25837e56dbbbf789d419f7e27551d559
koltluo <koltluo999@gmail.com>
2025-03-21 17:04:17 +0800

    去除多余文件

--- cloudflare-worker/src/alipayConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    appId: process.env.ALIPAY_APP_ID,
-    privateKey: process.env.ALIPAY_PRIVATE_KEY.replace(/\\n/g, '\n'),  // 处理换行符
-    alipayPublicKey: process.env.ALIPAY_PUBLIC_KEY.replace(/\\n/g, '\n'),
-    gateway: 'https://openapi.alipaydev.com/gateway.do',  // 沙箱环境
-    notifyUrl: 'https://your-worker-url.workers.dev/api/notify',  // 回调地址
-  };
